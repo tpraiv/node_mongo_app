2f91d4e26507d38e67377cbda501ef202ad88deb
tpraiv <tpraiv@ya.ru>
2022-08-07 15:03:08 +0300

    connection with mongodb and added a package of handlebars

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
-
-app.listen(PORT, () => {
-    console.log('Server has been started...')
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://tpraiv:MSde2e4B^@cluster0.zrdqg96.mongodb.net/todos', {
+            useNewUrlParser: true,
+            //useFindAndModify: false
+        })
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
 
 
 
