635201b2ea7b3258de0a7bc320f1a70188ca04ae
tpraiv <tpraiv@ya.ru>
2022-08-07 15:41:25 +0300

    added routes, some links and styles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 3000
 
@@ -13,6 +14,8 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(todoRoutes)
 
 async function start() {
     try {
